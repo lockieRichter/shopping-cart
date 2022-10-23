d70828579fe6c13b0edfafb6fc61a3a0004d9b72
Lockie Richter <richter.lockie@gmail.com>
2022-10-23 21:49:45 +1030

    Implement Google sign in for app

--- lib/main.dart
+++ lib/main.dart
@@ -1,84 +1,22 @@
 import 'package:flutter/material.dart';
-import 'package:shopping_list/views/menu_view.dart';
-import 'package:shopping_list/views/recipes_view.dart';
-import 'package:shopping_list/views/shopping_list_view.dart';
+
+import 'screens/sign_in.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'FlutterFire Samples',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.indigo,
+        brightness: Brightness.dark,
       ),
-      home: const MyHomePage(title: 'Menu Shop Aoo'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _selectedIndex = 0;
-
-  void _onItemTapped(int index) {
-    setState(() {
-      _selectedIndex = index;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: _buildCurrentView(),
-      bottomNavigationBar: _buildBottomNavigationBar(),
-    );
-  }
-
-  Widget _buildCurrentView() {
-    switch (_selectedIndex) {
-      case 0:
-        return const MenuView();
-      case 1:
-        return const ShoppingListView();
-      case 2:
-        return const RecipesView();
-    }
-    return const Text("TODO");
-  }
-
-  BottomNavigationBar _buildBottomNavigationBar() {
-    return BottomNavigationBar(
-      items: const <BottomNavigationBarItem>[
-        BottomNavigationBarItem(
-          icon: Icon(Icons.menu_book),
-          label: 'Menu',
-        ),
-        BottomNavigationBarItem(
-          icon: Icon(Icons.shopping_cart),
-          label: 'Shopping List',
-        ),
-        BottomNavigationBarItem(
-          icon: Icon(Icons.checklist),
-          label: 'Recipes',
-        ),
-      ],
-      currentIndex: _selectedIndex,
-      selectedItemColor: Colors.amber[800],
-      onTap: _onItemTapped,
+      home: SignInScreen(),
     );
   }
 }
